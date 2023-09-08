345f1318c869c2d82aa91bd15338edb3db20e259
Graps <88650896+GrapesGoober@users.noreply.github.com>
2023-09-08 10:05:43 +0700

    Create simplic.c with argv and file
    Did basic argv and file opening, but did not do much

---
+++ simplic.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    // read cli arguments to get the filepath
+    // cli should look like      smplc.exe 'file-to-compile'
+    if (argc != 2)
+    {
+        // filepath argument not given
+        fprintf(stderr, "Requires 1 argument: filepath");
+        return 1;
+    }
+
+    // start reading the file
+    FILE* sourcefile;
+    sourcefile = fopen(argv[1], "r");
+    if (sourcefile == NULL)
+    {
+        perror("Cannot open file");
+    }
+
+    // start parsing the file
+    // unimplemented
+
+    // write to hex file
+    // unimplemented
+
+    // close file as a good measure
+    closef(sourcefile);
+    return 0;
+}
