89e85c1406461beebc55d551028e2798f7a80f4d
Graps <88650896+GrapesGoober@users.noreply.github.com>
2023-09-15 16:19:15 +0700

    Update aofs-hexfile.h

--- simplic/aofs-hexfile.h
+++ simplic/aofs-hexfile.h
@@ -1,3 +1,3 @@
 // Simplic Compiler 2022, Nachat Kaewmeesang
-// utils-hexfile.h is the header file for hex file manipulation functions 
+// aofs-hexfile.h is the header file for hex file manipulation functions 
 // specifically, it provides utility for writing to hex file, and replacing an extention to .hex
