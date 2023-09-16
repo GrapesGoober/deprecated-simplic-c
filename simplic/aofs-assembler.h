b27fd5b93cbb3fbb9456c4646c5a4af0bd27dd04
Graps <twinklierre@gmail.com>
2023-09-16 22:18:05 +0700

    improved header file comments

--- simplic/aofs-assembler.h
+++ simplic/aofs-assembler.h
@@ -1,30 +1,83 @@
-// Simplic Compiler 2022, Nachat Kaewmeesang
-// aofs-assembler.h is the header file for the simplic assembler, which converts 
-// simplic assembly instructions (in string) into binary code (in uint16_t). 
-// All functions of simplic assembler is prefixed with aofs_ for clarity
+/**
+ * Simplic Compiler 2023, Nachat Kaewmeesang
+ * aofs-assembler.h is the header file for the simplic assembler. It contains parsers that
+ * converts simplic assembly instructions (in string) into binary code (in uint16_t). 
+ * All functions of simplic assembler is prefixed with aofs_ for clarity.
+*/
+
 #include <stdint.h>
+#include <stdbool.h>
 
-// Parses a single line of any assembly instruction to binary
-uint16_t aofs_ANY_tobinary(char *asmLine);
+/** 
+ * A global variable that contains error messages for all AOFs.
+ * This is only used for assembly syntax errors and hexfile errors.
+*/
+char g_aofs_error_msg[256];
 
-// Parses a hexadecimal literal to binary code with size restriction (caller must do appropriate shift left)
-uint16_t aofs_HEX_tobinary(char *hexliteral, uint8_t size);
+/** 
+ * Writes an assembly error message to `g_aofs_error_msg`.
+ * Arguments:
+ * - `char *mnem` Null terminated string containing the mnemonic(s) of the problem.
+ * - `char *message` Null terminated string containing the message.
+*/
+void aofs_asm_error(char *mnem, char *message);
 
-// Parses a register mnemonic to binary code (caller must do appropriate shift left)
-uint16_t aofs_REG_tobinary(char *mnem);
+/** 
+ * Parses an entire line of assembly instruction to binary. 
+ * Arguments:
+ * - `char *asmline` Null terminated string containing a line of assembly.
+ * - `uint16_t* bincode` The pointer to write the result binary code to.
+ * 
+ * Returns: `true` if success, `false` otherwise. 
+ * Note: if returns `false`, the error messages can be found in `g_aofs_error_msg`.
+*/
+bool aofs_ASM_tobinary(char *asmline, uint16_t *bincode);
 
-// Parses an entire line of MOV or MVS assembly instruction to binary code
-// These two instructions are collectively called "conditional" instructions, or CND
-uint16_t aofs_CND_tobinary(char *asmline);
+/** 
+ * Parses a hexadecimal literal to binary code (with size restriction)
+ * Arguments:
+ * - `char *hex_literal` Null terminated string containing 2 digits of hex.
+ * - `uint8_t size` Size restriction, in number of bits.
+ * - `uint16_t* bincode` The pointer to write the result binary code to.
+ * 
+ * Returns: `true` if success, `false` otherwise. 
+ * 
+ * Notes:
+ * - The caller must do appropriate shift left to place the binary code at the right location
+ * - If returns `false`, caller should disregard `bincode` and check `g_aofs_error_msg`.
+*/
+bool aofs_HEX_tobinary(char *hex_literal, uint8_t size, uint16_t *bincode);
 
-// Parses an entire line of MOV or MVS assembly instruction to binary code
-// These two instructions are collectively called "memory" instructions, or MEM
-uint16_t aofs_MEM_tobinary(char *asmline);
+/** 
+ * Parses a register mnemonic to binary code
+ * Arguments:
+ * - `char *reg_mnemonic` Null terminated string containing 2 digits of register mnemonic.
+ * - `uint16_t* bincode` The pointer to write the result binary code to.
+ * 
+ * Returns: `true` if success, `false` otherwise. 
+ * 
+ * Notes:
+ * - The caller must do appropriate shift left to place the binary code at the right location
+ * - If returns `false`, caller should disregard `bincode` and check `g_aofs_error_msg`.
+*/
+bool aofs_REG_tobinary(char *reg_mnemonic, uint16_t* bincode);
 
-// Parses an entire line of SFT assembly instruction to binary code
-// This instruction has a peculiar syntax, so it gets its own function
-uint16_t aofs_SFT_tobinary(char *asmline);
+/** 
+ * Parses conditional assembly instructions (`MOV` or `CNA`) to binary code.
+ * Arguments:
+ * - `char *asmline` Null terminated string containing a line of assembly.
+ * - `uint16_t* bincode` The pointer to write the result binary code to.
+ * 
+ * Returns: true if success, false otherwise.
+ * 
+ * Notes: 
+ * - Assembly arguments are in format `Rd Rn CND`. 
+ * - If returns `false`, caller should disregard `bincode` and check `g_aofs_error_msg`.
+*/
+bool aofs_CND_tobinary(char *asmline, uint16_t* bincode);
 
-// Parses an entire line of ALU assembly instruction to binary code
-// The ALU instructions includes ADD, SUB, MUL, LML, DIV, MOD, AND, ORR, XOR, NOR
-uint16_t aofs_ALU_tobinary(char *asmline);
+bool aofs_MEM__tobinary(char *asmline, uint16_t* bincode);
+
+bool aofs_SFT__tobinary(char *asmline, uint16_t* bincode);
+
+bool aofs_ALU__tobinary(char *asmline, uint16_t* bincode);
