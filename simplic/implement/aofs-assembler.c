fe1e96cd469920bb8608ba590adf8eedb11778dd
Graps <88650896+GrapesGoober@users.noreply.github.com>
2023-09-21 15:02:15 +0700

    Update aofs-assembler.c

--- simplic/implement/aofs-assembler.c
+++ simplic/implement/aofs-assembler.c
@@ -12,6 +12,7 @@
 
 #include "../aofs-assembler.h"
 
+// Register tokens
 const char REG_TOKENS[16][3] = {
     "ZR", "R1", "R2", "R3", "R4", "R5", // General Purpose Registers
     "R6", "R7", "R8", "R9", "RA", "RB", 
@@ -19,6 +20,22 @@ const char REG_TOKENS[16][3] = {
     "BR", // Buffer Register (for jumps, memory address, literal assignments etc)
     "LR", // Link Register (PC's last location to return to, after finished a call)
     "PC"  // Program Counter (points to the current instruction to read)
+};
+
+// Conditional tokens used by MOV and CNA
+const char CND_TOKENS[16][3] = {
+    // Set & Clear status for each flags
+    "ZS", "ZC", // Zero Flag for equality comparison
+    "CS", "CC", // Carry Flag, for handling arithmatics
+    "NS", "NC", // Negative Flag for more than & less than comparison
+    "VS", "VC", // Overflow flag for handling nasty arithmatics
+    // General comparison useful for branches
+    "SM", "HI", // Smaller & Higher, for unsigned, use with SUB
+    "LT", "GT", // Lesser & Greater, for signed, use with SUB
+    "LE", "GE", // LT & GT or Equal
+    "FC", // Flag Clear
+    // And of course...
+    "AL" // Always, does not bother with flags
 };
 
 void aof_asm_fmterror(char *mnem, char *message){
